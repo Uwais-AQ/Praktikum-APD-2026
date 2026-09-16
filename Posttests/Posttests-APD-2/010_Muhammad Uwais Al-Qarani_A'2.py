3a61397b5f46a0a184fb15f243061cad6fad8320
Muhammad Uwais A.Q <mhd.uwais.a.q@gmail.com>
2026-09-16 08:12:06 -0700

    Initial commit

---
+++ Posttests/Posttests-APD-2/010_Muhammad Uwais Al-Qarani_A'2.py
@@ -0,0 +1,12 @@
+barang_1 = 15000
+barang_2 = 25000
+barang_3 = 10000
+barang_4 = 5500
+barang_5 = 5000
+barang_6 = 20000
+
+list_barang = [barang_1, barang_2, barang_3, barang_4, barang_5, barang_6]
+
+subtotal = barang_1 + barang_2 + barang_3 + barang_4 + barang_5 + barang_6
+
+total_bayar = subtotal + (0.15 * subtotal)
